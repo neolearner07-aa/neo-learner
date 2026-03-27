ab0cf6717b7ec1742802ee97295814a72fe72526
Akhnas <neolearner07@gmail.com>
2026-03-27 21:56:17 +0530

    Added environment variables

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
 export default function Home() {
   return (
-    <main className="flex items-center justify-center h-screen">
+    <main className="flex flex-col items-center justify-center h-screen">
       <h1 className="text-3xl font-bold">
-        NeoLearner 🚀
+        {process.env.NEXT_PUBLIC_APP_NAME} 🚀
       </h1>
+      <p className="mt-2 text-gray-500">
+        Environment: {process.env.NEXT_PUBLIC_ENV}
+      </p>
     </main>
   );
 }
